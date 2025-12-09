f8e917cae9164c0f02b4f8fe8a9e614f76f8a3e3
obanext <93115438+obanext@users.noreply.github.com>
2025-12-09 13:14:48 +0100

    Add pagination support to agenda API handler

--- api/agenda.js
+++ api/agenda.js
@@ -1,13 +1,11 @@
 export default async function handler(req, res) {
   const key = process.env.OBA_API_KEY;
-  const { when = "b_thisweek", where = "" } = req.query;
-  const wanneerFacet = `Wanneer(${when})`;
-  let branchParam = "";
-  if (where !== "") {
-    branchParam = `&branch=${encodeURIComponent(where)}`;
-  }
+  const { when = "b_thisweek", where = "", page = "1" } = req.query;
 
-  const url = `https://zoeken.oba.nl/api/v1/search/?q=table:evenementen&refine=true&authorization=${key}&facet=${wanneerFacet}${branchParam}&sort=evt_dt_asc&pagesize=8`;
+  const wanneerFacet = `Wanneer(${when})`;
+  const branchParam = where !== "" ? `&branch=${encodeURIComponent(where)}` : "";
+
+  const url = `https://zoeken.oba.nl/api/v1/search/?q=table:evenementen&refine=true&authorization=${key}&facet=${wanneerFacet}${branchParam}&pagesize=8&page=${page}`;
 
   try {
     const response = await fetch(url);
