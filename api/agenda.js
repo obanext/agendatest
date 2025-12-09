bc6acb7f5db3d14e138b2f2b84b73468f8d572a7
obanext <93115438+obanext@users.noreply.github.com>
2025-12-09 11:39:10 +0100

    Update API request URL in agenda.js
    Removed sorting parameter from API request URL.

--- api/agenda.js
+++ api/agenda.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const key = process.env.OBA_API_KEY;
 
   const url =
-    `https://zoeken.oba.nl/api/v1/search/?q=table:evenementen&refine=true&authorization=${key}&branch=OBA%20Javaplein&facet=Wanneer(b_thisweek)&pagesize=8&sort=datumstart:asc`;
+    `https://zoeken.oba.nl/api/v1/search/?q=table:evenementen&refine=true&authorization=${key}&branch=OBA%20Javaplein&facet=Wanneer(b_thisweek)&pagesize=8`;
 
   try {
     const response = await fetch(url);
