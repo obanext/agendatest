b98bb297450d67ecac87072892d7efbff41d88bb
obanext <93115438+obanext@users.noreply.github.com>
2025-12-09 11:31:50 +0100

    Refactor API URL in agenda.js
    Refactor API URL construction for better readability and add pagination and sorting parameters.

--- api/agenda.js
+++ api/agenda.js
@@ -1,11 +1,19 @@
 export default async function handler(req, res) {
   const key = process.env.OBA_API_KEY;
-  const url = `https://zoeken.oba.nl/api/v1/search/?q=%20table:evenementen&refine=true&authorization=${key}&branch=OBA%20Javaplein&facet=Wanneer(b_thisweek)`;
+
+  const url =
+    `https://zoeken.oba.nl/api/v1/search/` +
+    `?q=table:evenementen` +
+    `&refine=true` +
+    `&authorization=${key}` +
+    `&branch=OBA%20Javaplein` +
+    `&facet=Wanneer(b_thisweek)` +
+    `&pagesize=8` +
+    `&sort=datumstart:asc`;
 
   try {
     const response = await fetch(url);
     const xml = await response.text();
-
     res.setHeader("Content-Type", "application/xml");
     res.status(200).send(xml);
   } catch (err) {
