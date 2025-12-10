cd85d27e43eab506a4d39c5a2dc77f4b0ccd918b
obanext <93115438+obanext@users.noreply.github.com>
2025-12-10 05:54:29 +0100

    Update agenda.js

--- api/agenda.js
+++ api/agenda.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   try {
     const data = await getAgendaForBranch(where);
     res.status(200).json(data);
-  } catch (err) {
+  } catch {
     res.status(500).json({ error: "Kan data niet ophalen" });
   }
 }
